2e6b36ce86ea11c658b879c64f385c8e81b6d107
Broomshakalaka <moofasaty@gmail.com>
2021-01-20 15:32:46 -0500

    epstein didnt kill himself

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# icon-marketplace
+## icon-marketplace
+
+# Author
+
+Spencer Dips
+
+# bibliography
+
+https://stackoverflow.com/questions/41370741/how-do-i-edit-a-css-variable-using-js for changing CSS variables (for dark mode feature)
---
+++ js/main.js
@@ -0,0 +1,9 @@
+function darkMode() {
+    document.documentElement.style.setProperty('--color1', 'white');
+    document.documentElement.style.setProperty('--color2', 'black');
+      }
+
+      function lightMode() {
+        document.documentElement.style.setProperty('--color1', 'black');
+        document.documentElement.style.setProperty('--color2', 'white');
+          }
