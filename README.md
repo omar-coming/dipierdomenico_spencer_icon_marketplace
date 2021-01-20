506f8ac80fd1cb649daa73d2e297fa23c8df5915
Broomshakalaka <moofasaty@gmail.com>
2021-01-20 16:26:16 -0500

    epstein didnt kill himself

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Spencer Dips
 # bibliography
 
 https://stackoverflow.com/questions/41370741/how-do-i-edit-a-css-variable-using-js for changing CSS variables (for dark mode feature)
+
+https://stackoverflow.com/questions/15241915/how-to-change-css-property-using-javascript (widescreen mode)
+
+https://www.w3schools.com/howto/howto_js_toggle_text.asp
--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 function darkMode() {
     document.documentElement.style.setProperty('--color1', 'white');
     document.documentElement.style.setProperty('--color2', 'black');
-      }
+    }
 
-      function lightMode() {
-        document.documentElement.style.setProperty('--color1', 'black');
-        document.documentElement.style.setProperty('--color2', 'white');
-          }
+function lightMode() {
+    document.documentElement.style.setProperty('--color1', 'black');
+    document.documentElement.style.setProperty('--color2', 'white');
+    }
+
+function changeBig() {
+    document.getElementById("grid-container").style.maxWidth="100%";
+    }
+
+
+function changeSmall() {
+    document.getElementById("grid-container").style.maxWidth="1200px";
+    }
+
+
+      function pdfBigger() {
+        var x = document.getElementById("infoSheet");
+        if (x.style.height === "25vh") {
+          x.style.height = "100vh";
+        } else {
+          x.style.height = "25vh";
+        }
+      } 
