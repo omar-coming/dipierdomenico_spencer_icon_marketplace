a1f31fbe4664b3b025c6b4cfd5086d353d3e80cd
Broomshakalaka <moofasaty@gmail.com>
2021-01-29 10:32:28 -0500

    scooby doo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
-function darkMode() {
-    document.documentElement.style.setProperty('--color1', 'white');
-    document.documentElement.style.setProperty('--color2', 'black');
+function lightMode() {
+    document.documentElement.style.setProperty('--color1', '#5BC2E7');
+    document.documentElement.style.setProperty('--color2', '#fff');
+    document.documentElement.style.setProperty('--color3', '#e75bbd');
+    document.documentElement.style.setProperty('--color4', '#a9e8ff');
+    document.documentElement.style.setProperty('--color5', '#000000');
     }
 
-function lightMode() {
-    document.documentElement.style.setProperty('--color1', 'black');
-    document.documentElement.style.setProperty('--color2', 'white');
-    }
+    function darkMode() {
+      document.documentElement.style.setProperty('--color1', '#222');
+      document.documentElement.style.setProperty('--color2', '#cecece');
+      document.documentElement.style.setProperty('--color3', '#8f3535');
+      document.documentElement.style.setProperty('--color4', '#555');
+      document.documentElement.style.setProperty('--color5', '#999');
+      }
+
+      function changeSize() {
+        var x = document.getElementById("grid-container");
+        if (x.style.maxWidth === "100%") {
+          x.style.maxWidth = "1200px";
+        } else {
+          x.style.maxWidth = "100%";
+        }
+      } 
 
 function changeBig() {
     document.getElementById("grid-container").style.maxWidth="100%";
